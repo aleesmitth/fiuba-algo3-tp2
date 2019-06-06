dfbddb2682ce3b99f7a2aaedf36e7b6a3fac8157
Nacho <ignaciolatorreap@gmail.com>
2019-06-05 22:49:19 -0300

    Jugador y Test correspondiente inicializacion con hacha de madera

---
+++ src/fiuba/algo3/tp2/Jugador.java
@@ -0,0 +1,17 @@
+package fiuba.algo3.tp2;
+
+public class Jugador {
+    private Constructor mesaDeCrafteo;
+    private Herramienta primerHachaDeMadera;
+
+    // Metodos
+
+    public Jugador(){
+        this.mesaDeCrafteo = new Constructor();
+        this.primerHachaDeMadera = this.mesaDeCrafteo.construir(new Madera(), new Hacha());
+    }
+
+    public Herramienta getPrimerHachaDeMadera(){
+        return this.primerHachaDeMadera;
+    }
+}
