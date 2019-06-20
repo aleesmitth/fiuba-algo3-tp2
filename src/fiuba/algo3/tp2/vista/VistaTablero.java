bbe6531fb18df618adc8609589cdaa47166c2222
ilineira <ilineira@github.com>
2019-06-20 15:58:58 -0300

    Modifico vista tablero

--- src/fiuba/algo3/tp2/vista/VistaTablero.java
+++ src/fiuba/algo3/tp2/vista/VistaTablero.java
@@ -4,7 +4,6 @@ package fiuba.algo3.tp2.vista;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.layout.GridPane;
-import javafx.scene.control.Label;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.paint.Color;
 
@@ -17,18 +16,18 @@ public class VistaTablero {
 
     private void dibujar(GridPane contenedorCentral) {
 
-        new Rectangle(100,100,Color.GRAY);
-
-        for(int i = 0; i< 20; i++) {
-            for(int j = 0; j<20; j++) {
-                contenedorCentral.add(new Rectangle(30,30,Color.GRAY),i,j);
+        for(int i = 0; i< 80; i++) {
+            for(int j = 0; j<60; j++) {
+                contenedorCentral.add(new Rectangle(10,10,Color.GRAY),i,j);
             }
         }
+        //Dibuja supuesto jugador.
+        contenedorCentral.add(new Rectangle(10,10,Color.RED),9,9);
 
         contenedorCentral.setMinSize(200, 200);
         contenedorCentral.setPadding(new Insets(10, 10, 10, 10));
-        contenedorCentral.setVgap(5);
-        contenedorCentral.setHgap(5);
+        contenedorCentral.setVgap(0);
+        contenedorCentral.setHgap(0);
         contenedorCentral.setAlignment(Pos.CENTER);
     }
 }
