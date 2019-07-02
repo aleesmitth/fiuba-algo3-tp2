73abdfd3c2d22df78b02e028db54b1d1c723feac
ilineira <ilineira@github.com>
2019-07-02 20:02:43 -0300

    Agrego comportamiento a botonConstruir

---
+++ src/fiuba/algo3/tp2/vista/Handlers/ButtonHandlers/BotonConstruirEventHandler.java
@@ -0,0 +1,21 @@
+package fiuba.algo3.tp2.vista.Handlers.ButtonHandlers;
+
+import fiuba.algo3.tp2.modelo.Entidad.Herramienta.Herramienta;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import fiuba.algo3.tp2.modelo.Juego.Juego;
+
+public class BotonConstruirEventHandler implements EventHandler<ActionEvent> {
+
+    Juego juego;
+
+    public BotonConstruirEventHandler(Juego juego){
+        this.juego = juego;
+    }
+
+    @Override
+    public void handle(ActionEvent actionEvent){
+        Herramienta herramienta = this.juego.construirHerramienta();
+        
+    }
+}
