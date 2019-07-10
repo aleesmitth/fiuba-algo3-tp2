a915bccf1ca01dc189be9854ac2751c7bc3ca7b8
Nacho <ignaciolatorreap@gmail.com>
2019-07-10 19:35:44 -0300

    Agregados sonidos, pantalla principal y golpear material

--- src/fiuba/algo3/tp2/vista/Handlers/OnKeyPressHandlers/AplicacionOnKeyPressEventHandler.java
+++ src/fiuba/algo3/tp2/vista/Handlers/OnKeyPressHandlers/AplicacionOnKeyPressEventHandler.java
@@ -10,7 +10,11 @@ import fiuba.algo3.tp2.vista.VistaInventarioMateriales;
 import fiuba.algo3.tp2.vista.VistaTablero;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class AplicacionOnKeyPressEventHandler implements EventHandler<KeyEvent> {
 
@@ -20,6 +24,7 @@ public class AplicacionOnKeyPressEventHandler implements EventHandler<KeyEvent> 
     private VistaInventarioMateriales vistaInventarioMateriales;
     private Juego juego;
     private VistaInventarioHerramientas vistaInventarioHerramientas;
+
 
     public AplicacionOnKeyPressEventHandler(Stage stage, BarraDeMenu menuBar, Juego juego, VistaTablero vistaTablero, VistaInventarioMateriales vistaInventarioMateriales, VistaInventarioHerramientas vistaInventarioHerramientas) {
         this.stage = stage;
@@ -32,6 +37,8 @@ public class AplicacionOnKeyPressEventHandler implements EventHandler<KeyEvent> 
 
     @Override
     public void handle(KeyEvent event) {
+        Media sound = new Media(new File("src/fiuba/algo3/tp2/vista/sounds/golpearMadera.mp3").toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);
 
         switch (event.getCode()) {
             case ESCAPE:
@@ -77,6 +84,7 @@ public class AplicacionOnKeyPressEventHandler implements EventHandler<KeyEvent> 
             case CONTROL:
                 this.juego.jugadorGolpea();
                 System.out.println("Golpear was pressed");
+                mediaPlayer.play();
                 this.vistaInventarioMateriales.actualizar();
                 this.vistaTablero.actualizar();
                 break;
